b4613b74559b2b077177812f68083e5765f7f7a0
Zoltan Urban <56049513+Zoltan95@users.noreply.github.com>
2023-10-02 16:22:42 +0200

    Fixed Some Visual Bugs and Added the Laravel Assessment to My projects

--- inc/pages/projects.php
+++ inc/pages/projects.php
@@ -8,41 +8,21 @@ include '../base/head.php';
             <div class="projects-container__holder">
                 <div class="projects-card">
                     <div class="projects-card__img">
-                        <a class="projects-card__codesample-link" href="https://github.com/Zoltan95/netmatters" target=”_blank”>Code Sample</a>
-                        <a class="projects-card__img-link" href="https://netmatters.zoltan-urban.netmatters-scs.co.uk/" target=”_blank”>
-                            <img src="../../img/projects/netmatters-reflection-page.png" alt="Picture of Netmatters Website Reflection Project">
+                        <a class="projects-card__codesample-link" href="https://github.com/Zoltan95/LaravelAssessment" target=”_blank”>Code Sample</a>
+                        <a class="projects-card__img-link" href="https://laravel-assessment.zoltan-urban.netmatters-scs.co.uk/" target=”_blank”>
+                            <img src="../../img/projects/laravel.png" alt="Picture of My Portfolio Website">
                         </a>
                     </div>
                     <div class="projects-card__text">
-                        <h3>Netmatters Website Reflection</h3>
-                        <p>HTML, CSS (SASS), JavaScript</p>
-                        <p>Develop the following website page and rebuild it using HTML and SASS.</p>
-                        <p>The site should be a mirror of the Netmatters homepage.</p>
-                        <a href="https://www.netmatters.co.uk/">https://www.netmatters.co.uk</a>
+                        <a class="link" href="https://laravel-assessment.zoltan-urban.netmatters-scs.co.uk/" target=”_blank”><h3>Laravel Assessment</h3></a>
+                        <p>HTML, Bootstrap, JavaScript, PHP (Laravel)</p>
+                        <p>Create a website using Laravel where information only can be viewed for a logged in user.
+                            The website must contain companies and their employees, an admin should be able to add, edit, remove any companies or employees.
+                        </p>
                         <br>
                         <br>
                         <div class="projects-card__text-date">
-                            <p>05/06/2023</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="projects-card">
-                    <div class="projects-card__img">
-                        <a class="projects-card__codesample-link" href="https://github.com/Zoltan95/portfolio" target=”_blank”>Code Sample</a>
-                        <a class="projects-card__img-link" href="#" target=”_blank”>
-                            <img src="../../img/projects/portfolio-page.png" alt="Picture of My Portfolio Website">
-                        </a>
-                    </div>
-                    <div class="projects-card__text">
-                        <h3>Portfolio First</h3>
-                        <p>HTML, CSS (SASS), JavaScript</p>
-                        <p>Create a "portfolio" website page using HTML and SASS.</p>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <div class="projects-card__text-date">
-                            <p>20/06/2023</p>
+                            <p>02/10/2023</p>
                         </div>
                     </div>
                 </div>
@@ -54,7 +34,7 @@ include '../base/head.php';
                         </a>
                     </div>
                     <div class="projects-card__text">
-                        <h3>JavaScript Array Assessment</h3>
+                        <a class="link" href="https://js-array.zoltan-urban.netmatters-scs.co.uk/" target=”_blank”><h3>JavaScript Array Assessment</h3></a>
                         <p>HTML, CSS (SASS), JavaScript</p>
                         <p>A JavaScript project (using jQuery and/or other frameworks) which loads an image from https://picsum.photos/ and displays it on a web page.</p>
                         <p>It has an interface that is to display all images assigned to each email address, the email address only displays once. On picture addition new picture can be generated using a button.</p>
@@ -62,6 +42,26 @@ include '../base/head.php';
                         <br>
                         <div class="projects-card__text-date">
                             <p>17/07/2023</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="projects-card">
+                    <div class="projects-card__img">
+                        <a class="projects-card__codesample-link" href="https://github.com/Zoltan95/netmatters" target=”_blank”>Code Sample</a>
+                        <a class="projects-card__img-link" href="https://netmatters.zoltan-urban.netmatters-scs.co.uk/" target=”_blank”>
+                            <img src="../../img/projects/netmatters-reflection-page.png" alt="Picture of Netmatters Website Reflection Project">
+                        </a>
+                    </div>
+                    <div class="projects-card__text">
+                        <a class="link" href="https://netmatters.zoltan-urban.netmatters-scs.co.uk/" target=”_blank”><h3>Netmatters Website Reflection</h3></a>
+                        <p>HTML, CSS (SASS), JavaScript</p>
+                        <p>Develop the following website page and rebuild it using HTML and SASS.</p>
+                        <p>The site should be a mirror of the Netmatters homepage.</p>
+                        <a href="https://www.netmatters.co.uk/">https://www.netmatters.co.uk</a>
+                        <br>
+                        <br>
+                        <div class="projects-card__text-date">
+                            <p>05/06/2023</p>
                         </div>
                     </div>
                 </div>
